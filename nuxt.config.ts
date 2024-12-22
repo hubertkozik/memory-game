51b197ef447ec751dae1e9929f48a17241eabd51
hubertkozik <hubertkozik13@gmail.com>
2024-12-22 18:57:38 +0100

    GH Actions update.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
-        baseURL: process.env.BASE_URL || '',
         head: {
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1',
